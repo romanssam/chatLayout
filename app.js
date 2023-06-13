c5a0c299cbe1c45638fa1b67bcef3ce05ffa79e4
romanssam <troubledyouthh@outlook.com>
2023-06-13 12:50:11 +0500

    searchbar
    searchbar

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const kebab = document.querySelector('[data-toggle="ChatBodyKebab"]')
 const kebabNode = document.querySelector('.chatKebab')
 
+const searchButton = document.querySelector('[data-toggle="ChatBodySearch"]')
+const searchNode = document.querySelector('.searchBar')
+
+const searchClose = document.querySelector('[data-toggle="searchBarClose"]')
+
 document.addEventListener('click', (e) => {
         if (!kebab.contains(e.target)) {
             kebabNode.classList.remove('shown');
@@ -8,6 +13,15 @@ document.addEventListener('click', (e) => {
     });
 
 
+
 kebab.addEventListener('click', event => {
   kebabNode.classList.toggle('shown');
+})
+
+searchButton.addEventListener('click', event => {
+  searchNode.classList.toggle('shown')
+})
+
+searchClose.addEventListener('click', () => {
+  searchNode.classList.remove('shown')
 })
